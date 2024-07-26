7ff2ca439d384806b67b6739f9201ee575cb10f4
Saikat Sundar Das <92164254+Saikatssd@users.noreply.github.com>
2024-07-27 01:19:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true
+  },
   resolve: {
     alias: {
       lodash: 'lodash-es',
     },
-  },
-  proxy: {
-    '/api': 'http://localhost:4000',
-  },
+  }
 })
 
